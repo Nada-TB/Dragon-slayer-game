0b3fb331380b4c0aa6d9e79fee59c418b829bd4c
Nada <50797024+Nada-TB@users.noreply.github.com>
2023-09-09 16:15:38 +0000

    Update Style.css and app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let play= document.querySelector('button');
 // click on play 
 play.addEventListener("click",function(){
     if(checkObjectValues(choices)==true){
-        preparation.classList.add('hide');
+        preparation.style.display="none";
         gameResult.classList.remove('hide');
         game();
     }else{
